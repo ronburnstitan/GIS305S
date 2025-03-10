e071447fdab26779360639b07639d21610db795e
rburn <ronald.burns@geoexcenter.org>
2025-03-09 20:45:20 -0600

    added try/except

--- assignment7/BasicMap.py
+++ assignment7/BasicMap.py
@@ -55,6 +55,13 @@ if __name__ == "__main__":
     my_map = BasicMap("-105.2705", "40.015", "0.5", "0.25")
     my_map.describe()
 
+    try:
+        print("Calculating bounds...")
+        my_map.get_bounds()
+    except TypeError:
+        print("Error: in get_bounds - Check your input values, they must be numbers!")
+
+
 
     print('Calculating bounds...')
     my_map.get_bounds()
